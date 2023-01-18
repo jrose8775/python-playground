7db2a120bea35e330c7a800a4c458d696ac8a8e7
Jordan Rose <jrose8775@gmail.com>
2023-01-17 20:10:28 -0400

    initial madlibs

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited because \
+I enjoy to {verb1}. Stay hibrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
